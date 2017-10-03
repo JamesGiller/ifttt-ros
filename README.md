eaf088fd6b1dc7cfee19813de32314fe8c61050d
James Giller <giller@bubblelab.com>
2017-10-03 17:58:52 +0900

    Create a rosnodejs-based test server.
    The server can make calls to the IFTTT Maker Webhooks service when a TriggerEvent is fired.

--- README.md
+++ README.md
@@ -8,11 +8,31 @@ The project is planned to consist of components that you can plug into any curre
 
 ## Cloning
 When cloning this repo directly into a catkin workspace, set the name of the resulting directory to ifttt as follows:
+
 `git clone https://github.com/JamesGiller/ifttt-ros.git ifttt`
 
 This is because I have named the package "ifttt" in the configuration files (CMakeLists.txt, package.xml).
 
+## Dependencie
+- [ROS Kinetic](http://wiki.ros.org/kinetic/Installation/Ubuntu)
+- [Node.js](https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions) (v8.6.0)
+- [NPM](https://docs.npmjs.com/getting-started/installing-node) (5.4.2)
+
+Also check dependencies of [rosnodejs](http://wiki.ros.org/rosnodejs#Installation) relating to ROS.
+
+## Building
+1. Install dependencies from package.json
+
+`npm install`
+
+2. Build with catkin
+
+`catkin build`
+
 ## Testing
 To run the tests, you must install the source of Google Mock.
 This can be done on Ubuntu 16.04 by executing the following:
+
 `sudo apt install google-mock`
+
+See [the catkin docs](http://catkin-tools.readthedocs.io/en/latest/verbs/catkin_build.html#building-and-running-tests) for how to run tests.
---
+++ scripts/test_server.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var request = require('request');
+
+const rosnodejs = require('rosnodejs');
+const ifttt_msgs = rosnodejs.require('ifttt').msg;
+
+const MAKER_KEY_PARAM = "/maker_key";
+const TRIGGER_NAMES_PARAM = "/trigger_names";
+
+function fetchParams(nodeHandle) {
+  // var fetchMakerKey = nodeHandle.hasParam(MAKER_KEY_PARAM)
+  //   .then(exists => {
+  //     if (exists) {
+  //       return nodeHandle.getParam(MAKER_KEY_PARAM);
+  //     } else {
+  //       return Promise.reject(`Parameter ${MAKER_KEY_PARAM} is required`);
+  //     }
+  //   });
+  // There is a bug in hasParam in current version of rosnodejs
+  // Submitted bugfix PR (https://github.com/RethinkRobotics-opensource/rosnodejs/pull/71); waiting on result.
+  var fetchMakerKey = nodeHandle.getParam(MAKER_KEY_PARAM);
+
+  var fetchTriggerNames = nodeHandle.getParam(TRIGGER_NAMES_PARAM);
+
+  return Promise.all([fetchMakerKey, fetchTriggerNames, nodeHandle]);
+};
+
+function TriggerHandler(makerKey, triggerName, nodeHandle) {
+  this.subscriber = nodeHandle.subscribe(triggerName, ifttt_msgs.TriggerEvent,
+    trigger => {
+      rosnodejs.log.info(`Trigger [${triggerName}]: Firing`);
+      request(
+        {
+          method: 'POST',
+          uri: `https://maker.ifttt.com/trigger/${triggerName}/with/key/${makerKey}`
+        },
+        (error, response, body) => {
+          if (response.statusCode != 200) {
+            rosnodejs.log.error(`Trigger [${triggerName}]: Error [${response.statusCode} ${body}]`);
+          }
+        }
+      );
+    });
+}
+
+function constructTriggerHandlers([makerKey, triggerNames, nodeHandle]) {
+  var handlers = new Array();
+  triggerNames.forEach(triggerName => {
+    handlers.push(new TriggerHandler(makerKey, triggerName, nodeHandle));
+  });
+  return Promise.resolve(handlers);
+}
+
+function testServer() {
+  rosnodejs.initNode('/test_server').then(fetchParams).then(constructTriggerHandlers)
+    .then(triggerHandlers => {
+      rosnodejs.log.info(`Server: Triggers [${triggerHandlers.length}]`);
+    })
+    .catch(reason => {
+      rosnodejs.log.error(`Server: Error [${reason}]`);
+      if (reason instanceof Error) {
+        rosnodejs.log.error(`Server: Trace [${reason.stack}]`);
+      }
+      process.exit(1);
+    });
+}
+
+if (require.main === module) {
+  testServer();
+}
